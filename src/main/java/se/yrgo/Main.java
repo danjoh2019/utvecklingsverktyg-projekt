248048322aa6661deb43cc05b86f0c2ffc4e1d63
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-02-07 09:41:42 +0100

    added comments about what we need in main

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -1,15 +1,23 @@
 package se.yrgo;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
+            // Skapa ett nytt spel
 
+            // Metod för att få fram ett slumpat ord
+            String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
             System.out.println(rndWord);
+
+            // Be om två spelarnamn
+            // Skapa två spelare
+
+            // Börja ett nytt spel
+
+            // Bäst av tre omgångar vinner?
 
         } catch (IOException ex) {
             System.err.println("Something went wrong");
