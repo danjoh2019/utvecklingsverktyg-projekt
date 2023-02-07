dbd8277dc0f1cbf74a8afd7e4395c8c8e6a0758e
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-02-07 08:23:22 +0100

    created a method to generate a random word

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -1,12 +1,20 @@
 package se.yrgo;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = Path.of("wordlist.txt");
-        String rndWord = WordGenerator.getWord(path);
 
-        System.out.println(rndWord);
+        try {
+            String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
+
+            System.out.println(rndWord);
+
+        } catch (IOException ex) {
+            System.err.println("Something went wrong");
+            ex.printStackTrace();
+        }
+
     }
 }
--- src/main/java/se/yrgo/WordGenerator.java
+++ src/main/java/se/yrgo/WordGenerator.java
@@ -1,13 +1,21 @@
 package se.yrgo;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WordGenerator {
 
-    public static String getWord(Path path) {
-        String word;
+    public static String getWord(String path) throws IOException {
+        String word = null;
+        List wordList = Files.readAllLines(Path.of(path));
+        int rndNumber = ThreadLocalRandom.current().nextInt(1, wordList.size() + 1);
 
-        word = "tt";
+        for (int i = 0; i < rndNumber; i++) {
+            word = wordList.get(i).toString();
+        }
 
         return word;
     }
