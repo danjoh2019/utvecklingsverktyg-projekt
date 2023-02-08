d2458042bfd369fc9b16bb8669b11ac31f5dc1aa
DaniloSchu <dansch0621@skola.goteborg.se>
2023-02-08 13:15:00 +0100

    Update main class

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -1,20 +1,30 @@
 package se.yrgo;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        try {
+        try (Scanner scanner = new Scanner(System.in)){
             // Skapa ett nytt spel
 
             // Metod för att få fram ett slumpat ord
-            String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
-            System.out.println(rndWord);
 
-            // en spelklass
-            // en playerklass
-            //
+
+            Player player1 = new Player(input);
+            Player player2 = new Player(input);
+
+            for (int i = 0; i < 3; i++) {
+                String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
+                Game game = new Game(player1, player2, rndWord);
+                String winner = game.startGame();
+
+                // Bäst av tre omgångar vinner?
+                System.out.println("Spelet är över");
+                System.out.printf("%s vann!", winner);
+            }
+
 
             // Be om två spelarnamn
             // Skapa två spelare
@@ -22,7 +32,7 @@ public class Main {
 
             // Börja ett nytt spel
 
-            // Bäst av tre omgångar vinner?
+
 
         } catch (IOException ex) {
             System.err.println("Something went wrong");
