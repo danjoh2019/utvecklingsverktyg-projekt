b09164d4c4bc59aefe4a6e2dbb994847c8a7b58e
antonwilen <antwil1107@skola.goteborg.se>
2023-02-08 13:42:34 +0100

    Updated Game and Main class

--- src/main/java/se/yrgo/Game.java
+++ src/main/java/se/yrgo/Game.java
@@ -2,6 +2,7 @@ package se.yrgo;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -16,66 +17,62 @@ public class Game {
     private List<Character> guessedLetters;
 
 
-    public Game(Player playerOne, Player playerTwo, String randomWord){
+    public Game(Player playerOne, Player playerTwo, String randomWord) {
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
-        this.RANDOM_WORD_LIST = RANDOM_WORD_LIST;
         this.randomWord = randomWord;
+        this.guessedLetters = new ArrayList<>();
     }
 
-    public Player startGame() throws IOException {
-        running = true;
-        while(running){
-            randomWord = WordGenerator.getWord(RANDOM_WORD_LIST.toString());
-            if(ThreadLocalRandom.current().nextBoolean()){
-                currentPlayer = playerOne;
-            }
-            else {
-                currentPlayer = playerTwo;
-            }
-            System.out.println(GetSecretWord());
-            System.out.println(currentPlayer.getPlayer + "! Guess a letter or the entire word:");
-            String guess = scanner.next();
-            Guess(guess);
+    public void setRandomPlayer() {
+        if (ThreadLocalRandom.current().nextBoolean()) {
+            currentPlayer = playerOne;
+        } else {
+            currentPlayer = playerTwo;
         }
-
     }
+
+    public void gameLoop() {
+        System.out.println(GetSecretWord());
+        System.out.println(currentPlayer.getPlayer() + "! Guess a letter or the entire word:");
+    }
+
 
     private String GetSecretWord() {
         String output = "";
-        for (int i = 0; i < randomWord.length(); i++){
-            if(guessedLetters.contains(randomWord.charAt(i))){
+        for (int i = 0; i < randomWord.length(); i++) {
+            if (guessedLetters.contains(randomWord.charAt(i))) {
                 output += randomWord.charAt(i);
-            }
-            else {
+            } else {
                 output += "_";
             }
         }
         return output;
     }
 
-    private void Guess(String guess) {
-        if(guess.length() != randomWord.length()){
+    public void guess(String guess) {
+
+        if (guess.length() != randomWord.length()) {
             String guessedLetter = Character.toString(guess.charAt(0));
-            if(randomWord.contains(guessedLetter) && !(guessedLetters.contains(guessedLetter))){
+            if (randomWord.contains(guessedLetter) && !(guessedLetters.contains(guessedLetter))) {
                 guessedLetters.add(guessedLetter.charAt(0));
+                System.out.println("You guessed " + guessedLetter);
+            } else {
+                System.out.println("Invalid guess, try again!");
             }
-            else{
-                // invalid guess
+        } else {
+            if (guess.equals(randomWord)) {
+                //win(currentPlayer);
             }
         }
-        else{
-            if(guess.equals(randomWord)){
-                win(currentPlayer);
-            }
-        }
+
+
     }
 
-    private void switchPlayer(){
-        if (currentPlayer.equals(playerOne)){
+    public void switchPlayer() {
+        if (currentPlayer.equals(playerOne)) {
             currentPlayer = playerTwo;
-        }
-        else{
+        } else {
             currentPlayer = playerOne;
         }
     }
--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -11,19 +11,33 @@ public class Main {
 
             // Metod för att få fram ett slumpat ord
 
+            System.out.println("Vad heter spelare 1?");
+            Player player1 = new Player(scanner.next());
+            System.out.println("Vad heter spelare 2?");
+            Player player2 = new Player(scanner.next());
 
-            Player player1 = new Player(input);
-            Player player2 = new Player(input);
 
+            for(int i = 0; i < 3; i++){
+                String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
+                Game game = new Game(player1,player2,rndWord);
+                game.setRandomPlayer();
+                while (true){
+                    game.gameLoop();
+
+                    game.guess(scanner.next());
+                    game.switchPlayer();
+                }
+            }
+            /*
             for (int i = 0; i < 3; i++) {
                 String rndWord = WordGenerator.getWord("src/main/java/se/yrgo/wordlist.txt");
                 Game game = new Game(player1, player2, rndWord);
-                String winner = game.startGame();
+                //String winner = game.startGame();
 
                 // Bäst av tre omgångar vinner?
                 System.out.println("Spelet är över");
-                System.out.printf("%s vann!", winner);
-            }
+                //System.out.printf("%s vann!", winner);
+            }*/
 
 
             // Be om två spelarnamn
