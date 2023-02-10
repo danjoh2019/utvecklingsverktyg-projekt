fd0e25a87f30a612c85b80f7efda1efd33477ce8
DaniloSchu <dansch0621@skola.goteborg.se>
2023-02-10 11:57:06 +0100

    update jenkins trigger

--- src/main/java/se/yrgo/Player.java
+++ src/main/java/se/yrgo/Player.java
@@ -1,13 +1,18 @@
 package se.yrgo;
 
 /**
- * Her we input the player's name and generate points to the players.
+ * Her we input the player's name  and generate points to the players.
  */
 public class Player {
+
     private final String name;
 
     private int point;
 
+    /**
+     *
+     * @param player
+     */
     public Player(String player) {
         this.name = player;
         this.point = 0;
